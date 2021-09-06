93d98e531d01e5a3a6fd64cb63bebd2936d31ae8
unknown <jjdate@live.com>
2021-09-06 00:50:38 -0400

    axios call added

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [currency, setCurrency] = useState([]);
+  const [text, setText] = useState("");
+
+  const apiCall = async () => {
+    await axios
+      .get(
+        "https://v6.exchangerate-api.com/v6/fa3aa2e8d16ca8b28ef9783f/latest/USD"
+      )
+      .then((res) => {
+        console.log(res.data);
+        setCurrency({ ...res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{currency}</Text>
+      <TextInput
+        placeholder="Enter Code"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
+      <Button title="Click" onPress={() => apiCall()} />
     </View>
   );
 }
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
